ff646664d1627c81c4fcab55cd6713026c8c8208
Alysson Miller <alyssonmillerdev@gmail.com>
2024-03-27 09:47:45 +0000

    Ending day 7

--- day 7/script.js
+++ day 7/script.js
@@ -13,28 +13,59 @@
 //     dividir pelo número de alunos!
 
 
-let s = true;
+let a = true;
+let students = 0;
+let n = 0;
 let score 
 let gender
-let signup
-let notas
+let signUp
+let avg
+let men = 0;
+let w7 = 0;
+let highM = 0;
 
-while (s) {
-    score = prompt("Insert the score");
-    gender = prompt("Insert the gender");
-    signup = prompt("Signup again ?");
+while (a) {
+    score = parseInt(prompt("Insert a valid score"));
+    gender = prompt("Insert your gender");
+    signUp = prompt("Signup again ?");
+    
+    students++
+    n += score;
 
-    score++
-    notas = score;
+    avg = n / students;
 
-    console.log(notas);
+    switch (gender) {
+        case "m":
+        men++  
+        
+        if (score > highM) {
+            highM = score;
+        }
 
+        break;
 
-
-
-
-    if(signup === "n"){
-        s = false;
+        case "w":
+            if(score >= 7){
+                w7++
+            }
+            break;
+    
+        default:
+            break;
     }
+
+    if(signUp === "n"){
+        a = false
+    }
+
 }
+
+console.log("Quantity of students: " + students);
+console.log("Avarege score: " + avg);
+console.log("Quantity of men: " + men);
+console.log("Quantity of women more than seven: " + w7)
+console.log("This is the highest score betwen men: " + highM)
+
+
+
 
