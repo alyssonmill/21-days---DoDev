6030d9b6ea484da8cb10f0fd4b9cbb66b73621b1
Alysson Miller <alyssonmillerdev@gmail.com>
2024-03-29 09:42:28 +0000

    Ending day 7

--- day 7/ex1.js
+++ day 7/ex1.js
@@ -1,18 +1,3 @@
-// Desenvolva um algoritmo que receba a nota e o sexo do aluno, usando a estrutura WHILE, e pergunte ao usuario se 
-// ele deseja cadastrar outra nota. No final, exiba no console:
-
-
-//  - A média geral dos alunos
-//  - Quantos homens enviaram as notas
-//  - Quantas mulheres tiveram nota acima de 7
-//  - Qual a maior nota entre os homens
-    
-
-//     *Obs.: para obter a média geral deve somar todas as notas e 
-
-//     dividir pelo número de alunos!
-
-
 let a = true;
 let students = 0;
 let n = 0;
