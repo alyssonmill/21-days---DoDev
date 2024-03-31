73c977203b7655fcb70d7f8709e62fe545d1b156
Alysson Miller <alyssonmillerdev@gmail.com>
2024-03-31 23:08:55 +0100

    Ending day 9

--- day 9/script.js
+++ day 9/script.js
@@ -30,53 +30,30 @@ ageDays = 365 * userAge;
 
 imc = userWeight / (userHeight * userHeight);
 // let bmi = Number(imc.toFixed(2));
-let bmi = 22;
+let bmi = 50;
 
 
-switch (bmi) {
-    case bmi:
-        if (bmi <= 18.5) {
-            console.log("Magro")
-        }else{
-            console.log("Normal")
-        }
-    break;
-    case bmi:
-        if (bmi > 18.5 ) {
-            
-        }
-    break;
-    // case bmi:
-    //     if (bmi <= 18.5) {
-    //         console.log("Magro")
-    //     }
-    // break;
-    // case bmi:
-    //     if (bmi <= 18.5) {
-    //         console.log("Magro")
-    //     }
-    // break;
-
-    //  case (18.5 <= 24.9):
-
-    //     console.log("Magro") 
-
-    //     // if ( bmi <= 18.5 ) {
-    //     //     console.log("Magro")
-    //     // }
-    //     break;
-    default:
-        break;
+if (bmi <= 18.5 )  {
+    console.log("Skine")
+}else if (bmi > 18.5 && bmi <= 22.9)  {
+    console.log("Normal")
+}else if (bmi <= 30)  {
+    console.log("Fat")
+}else {
+    console.log("Very fat")
 }
 
-// console.log("Name: " + userName + " Age: " + userAge + " Wheight: " + userWeight + " Heigth: " 
-// + userHeight + " Job: " + userJob);
 
-// console.log("This is your age in months: " + ageMonth);
 
-// console.log("This is your age in weeks: " + ageWeek);
 
-// console.log("This is your age in days: " + ageDays);
+console.log("Name: " + userName + " Age: " + userAge + " Wheight: " + userWeight + " Heigth: " 
++ userHeight + " Job: " + userJob);
+
+console.log("This is your age in months: " + ageMonth);
+
+console.log("This is your age in weeks: " + ageWeek);
+
+console.log("This is your age in days: " + ageDays);
 
 console.log("This is your bmi: " + bmi);
 
