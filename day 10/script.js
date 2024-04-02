49e3cdbdbab48b36204063cac7202511e655ad23
Alysson Miller <alyssonmillerdev@gmail.com>
2024-04-02 06:07:41 +0100

    Starting day 10

---
+++ day 10/script.js
@@ -0,0 +1,33 @@
+////////////////////1////////////////////
+
+// let condition = true;
+// let numbers = [];
+// let userInsert 
+// let position = 0;
+// let ahead
+
+// while (condition) {
+//     userInsert = prompt("Insert a number");
+//     numbers[position] = userInsert;
+
+//     ahead = prompt("Insert 1 to ahead");
+//     if (ahead != "1") {
+//         condition = false;
+//     }
+
+//     position++
+// }
+
+////////////////////2////////////////////
+
+// let numbers = [0,1,2,3,4,5,6,7,8,9];
+
+// for(let i = numbers[9];i >= numbers[0]; i--){
+//     console.log(i)
+// }
+
+////////////////////3////////////////////
+
+// 3 - Refaça o script anterior, mas agora pergunte ao usuário quantos números ele deseja inserir no array. Depois, o usuário vai 
+// preenchendo elemento por elemento do vetor, ao final, exiba o array e o array ao contrário
+
