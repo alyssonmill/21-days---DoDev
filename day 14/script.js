092be844b03daa1d1944ed9a061364659e1f14bd
Alysson Miller <alyssonmillerdev@gmail.com>
2024-04-12 07:51:24 +0100

    Ending day 14

--- day 14/script.js
+++ day 14/script.js
@@ -1,15 +1,3 @@
-// Depois crie uma função que deve ser chamada dentro da função que solicita as 
-// informações, essa função irá calcular o aumento de salário conforme a seguinte 
-// tabela:
-//  ATÉ R$1.500,00 : aumento de 20%
-
-// R$1.501,00 até R$2.000,00 : aumento de 15%
-
-// R$2.001,00 até R$3.000,00 : aumento de 10%
-
-// R$3.001,00 em diante : aumento de 5%
-
-
 function info() {
     let userName = prompt("Insert a name");
     let userSalary = parseFloat(prompt("Insert ur salary:"));
@@ -17,15 +5,58 @@ function info() {
     let percent
 
     function increase(){
-        if (userSalary < 1500) {
+
+        if (userSalary <= 1500) {
             percent = 20/100
             increasing = percent * userSalary;
             console.log(userName +",This is your new salary: "+ "$" +(increasing + userSalary))
+        }else if (userSalary >= 1501 && userSalary <= 2000){
+            percent = 15/100
+            increasing = percent * userSalary;
+            console.log(userName +",This is your new salary: "+ "$" +(increasing + userSalary))
+        }else if (userSalary >= 2001 && userSalary <= 3000){
+            percent = 10/100
+            increasing = percent * userSalary;
+            console.log(userName +",This is your new salary: "+ "$" +(increasing + userSalary))
+        } else if (userSalary > 3001){
+            percent = 5/100
+            increasing = percent * userSalary;
+            console.log(userName +",This is your new salary: "+ "$" +(increasing + userSalary))
         }
+
+        function again (){
+            let again = prompt("Insert 1 to reapeat the process")
+            if (again === "1") {
+                info();
+            }else {
+                alert ("Thanks !!")
+                return;
+            }
+        }
+
+        again ()
     }
     
     increase()
 }
+
+// epois de calcular o aumento você deve exibir: o nome do colaborador, o salário, a % de aumento e o salário reajustado.
+
+
+// 4 - Crie uma função que  deve ser chamada no final da função que calcula o reajuste salarial, essa função deve perguntar se o usuário deseja calcular novamente com 
+// novas informações.
+
+
+// 5 - Para testar basta chamar a primeira função criada, e as outras serão chamadas na sequência conforme a execução do código
+
+
+//  * Para calcular o aumento, faça o valor do salário multiplicado
+
+//    pela porcentagem em decimal. 
+
+//      Ex.: salário R$1.000,00 aumento de 10%   |   salário R$1.000,00 aumento de 15%
+
+//           1000 * 1.10 = 1100                  |   1000 * 1.15 =   1150
 
 info();
 
