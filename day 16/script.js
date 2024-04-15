b73b097ca493c51fe983328117fa4e17084206b5
Alysson Miller <alyssonmillerdev@gmail.com>
2024-04-15 13:24:03 +0100

    Ending day 16

--- day 16/script.js
+++ day 16/script.js
@@ -3,71 +3,70 @@ let pass = [];
 let position = 0;
 let userName
 let userPass
+let condition = true;
 
 function info (){
     let op = prompt("1-register 2-login 3-delete 4-quit  ");
-
-    switch (op) {
-        case "1":
-            userName = prompt("Insert a name")
-            userPass = prompt("Insert a pass")
-            register(userName, userPass);
-            break;
-        case "2":
-            userName = prompt("Insert a name")
-            userPass = prompt("Insert a pass")
-            login(userName, userPass);            
-            break;
-        case "3":
-            userName = prompt("Insert a name")
-            userPass = prompt("Insert a pass")
-            delet(userName);            
-            break;
-        case "4" :
-            break;
-        default:
-            break;
-    }
-
     return op;
 }
 
-function register (){
+function register(){
+    userName = prompt("Insert a name");
+    userPass = prompt("Insert a pass");
+
     let op2 = prompt("Insert 1 to come back to the menu");
 
-    names[position] = userName;
-    pass[position] = userPass;
+    names.push(userName);
+    pass.push(userPass);
 
     if (op2 === "1") {
         info();
     }
-    position++
 }
 
 function login (userName, userPass){
-    if (names.includes(userName) === true && pass.includes(userPass) === true) {
+
+    if (names.includes(userName) && pass.includes(userPass)) {
         alert("Welcome")
-        info();
     }else {
         alert("Insert a valid info")
     }
 }
 
-function delet (userName){
+function delet (){
+    userName = prompt("Insert a name")
     let i = names.indexOf(userName)
 
     if(userName !== -1){
-        names.splice(i) 
-        pass.splice(i)
+        names.splice(i, 1) 
+        pass.splice(i, 1)
         alert("You have deleted")
+        info();
     }else {
         alert("I could not find this name")
     }
 }
 
+while (condition) {
+    let option = info();
 
-
-
-info();
+    switch (option) {
+        case "1":
+            register();
+            break;
+        case "2":
+            userName = prompt("Insert a name");
+            userPass = prompt("Insert a pass");
+            login(userName, userPass);
+            break;
+        case "3":
+            delet();            
+            break;
+        case "4" :
+            condition = false;
+        default:
+            break;
+    }
+}
 
 
